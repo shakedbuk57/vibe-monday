78838f131e4235ff2c066944e8b91641fcacda17
shakedbuk57 <shaked@autonomyai.io>
2025-12-18 22:23:59 +0200

    remnove codemod

--- packages/codemod/src/utils/index.ts
+++
@@ -1,5 +0,0 @@
-export { default as wrap } from "./wrap-transformation";
-export * from "./import-utils";
-export * from "./component-jsx-utils";
-export * from "./prop-utils";
-export * from "./report-utils";
--- packages/codemod/types/transformation-context.types.ts
+++
@@ -1,7 +0,0 @@
-import { Core, JSCodeshift } from "jscodeshift";
-
-export type TransformationContext = {
-  j: JSCodeshift;
-  root: ReturnType<Core>;
-  filePath: string;
-};
